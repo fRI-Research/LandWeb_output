ab4055c6053369f1867846bd62be140416842201
Alex Chubaty <alex.chubaty@gmail.com>
2018-12-13 14:07:33 -0700

    mean TSF plot:  add TODO
    is stacked plot the best/cleanest way to show this?

--- LandWeb_output.R
+++ LandWeb_output.R
@@ -117,7 +117,7 @@ doEvent.LandWeb_output <- function(sim, eventTime, eventType, debug = FALSE) {
     gg_tsfOverTime <- ggplot(mod$tsfOverTime,
                              aes(x = time, y = meanAge, fill = FRI, ymin = 0)) +
       geom_area() +
-      theme(legend.text = element_text(size = 6))
+      theme(legend.text = element_text(size = 6)) ## TODO: is a stacked plot the best way to show these!?
 
     Plot(gg_tsfOverTime, title = "Average age (TSF) by FRI polygon", addTo = "ageOverTime")
 
