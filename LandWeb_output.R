a43918f6d8ccb4a0d020d9f86a0432988526f9fa
Eliot McIntire <eliotmcintire@gmail.com>
2018-12-24 12:06:05 -0800

    SpaDES.core version minimum

--- LandWeb_output.R
+++ LandWeb_output.R
@@ -8,7 +8,7 @@ defineModule(sim, list(
     person(c("Alex", "M."), "Chubaty", email = "achubaty@friresearch.ca", role = c("ctb"))
   ),
   childModules = character(0),
-  version = numeric_version("1.3.2"),
+  version = list(SpaDES.core = "0.2.3.9009", LandWeb_output = numeric_version("1.3.2")),
   spatialExtent = raster::extent(rep(NA_real_, 4)),
   timeframe = as.POSIXlt(c(NA, NA)),
   timeunit = "year",
